1fec7457fc25e432587c0b16bb9a5d2591557913
AladdinOmar61 <Aladdin.Omar61@gmail.com>
2020-03-19 14:04:30 -0400

    added css to services

--- src/components/Services.js
+++ src/components/Services.js
@@ -4,11 +4,13 @@ function Services() {
   return (
     <div className="services">
       <h1>Our Services</h1>
-      <p>
+      <div className="services-div">
+      <p className="services-p">
         Tell us your story, and we'll help you reach new chapters. We will help
         you with creating a website that intrigues the modern eye, with design
         patterns that are simple but very pleasing and easy to navigate through.
       </p>
+      </div>
     </div>
   );
 }
